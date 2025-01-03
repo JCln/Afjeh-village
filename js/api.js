15cd24d553cdc6908c0274245517604c34815677
JCln <ghmantera@gmail.com>
2025-01-04 02:55:58 +0330

    14031015

---
+++ js/api.js
@@ -0,0 +1,13 @@
+axios.defaults.baseURL = 'http://130.185.75.117/KontoriNew';
+const bankUrl = 'https://sep.shaparak.ir/OnlinePG/SendToken?token=';
+
+async function getPayment() {
+    try {
+        const response = await axios.get('V1/Pay/Saman?resNum=54321');
+        console.log(response);
+        window.location.href = `${bankUrl + response.data.token}`;
+    } catch (error) {
+        console.error(error);
+    }
+
+}
