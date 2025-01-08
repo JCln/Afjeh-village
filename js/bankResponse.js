5bdb1a63410c8b573ea3d418d3289c6d504ea39b
JCln <ghmantera@gmail.com>
2025-01-08 20:55:58 +0330

    ui ux

---
+++ js/bankResponse.js
@@ -0,0 +1,11 @@
+"use strict"
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('The page has loaded!');
+    // const a = {
+        
+    // }
+    // axios.defaults.baseURL = 'https://130.185.75.117/KontoriNew';
+    // const bankUrl = 'https://sep.shaparak.ir/OnlinePG/SendToken?token=';
+
+});
